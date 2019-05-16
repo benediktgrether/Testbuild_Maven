f7d3373f8f51c74f93bb0bee08f02653dd312041
Benedikt Grether <info@benediktgrether.de>
2019-05-16 17:09:55 +0200

    Exercise 7

--- src/test/java/de/hfu/QueueTest.java
+++ src/test/java/de/hfu/QueueTest.java
@@ -7,7 +7,7 @@ public class QueueTest {
 	@Test
 	public void testQueue(){
 		Queue q = new Queue(3);
-		q.enqueue(5);
-		assertTrue(5 == q.dequeue());
+		q.enqueue(3);
+		assertTrue(3 == q.dequeue());
 	}
 }
